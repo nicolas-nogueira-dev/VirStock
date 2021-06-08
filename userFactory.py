87481ffa3ce57571f5755c08db14fa457d081a26
nicolas-nogueira-dev <71648696+RacoonLover@users.noreply.github.com>
2021-06-08 18:53:30 +0200

    Work Update

--- userFactory.py
+++
@@ -1,7 +0,0 @@
-from werkzeug.security import generate_password_hash
-
-name = input("name : ")
-sercure_password = generate_password_hash(input("password : "))
-
-with open(".users", "a") as f:
-    f.write(f"{name}|{sercure_password}\n")
